b6fb2f3dbe2510e2a8136a89c8a092cfc5a30615
Hector Anton <hectoranf@gmail.com>
2020-05-15 10:40:38 +0200

    deploying

--- server/app.js
+++ server/app.js
@@ -16,13 +16,13 @@ require('./configs/passport.config')(app)
 require('./configs/views.configs')(app)
 require('./configs/locals.config')(app)
 
-// app.use((req, res) => {
-//     res.sendFile(__dirname + "/public/index.html")
-// })
-
 // Base URLS
 app.use('/api', require('./routes/auth.routes'))
 app.use('/api/meeting', require('./routes/meeting.routes'))
 app.use('/api/media', require('./routes/media.routes'))
+
+app.use((req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
 
 module.exports = app
